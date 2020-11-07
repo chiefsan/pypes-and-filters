79601ecf551fc86529ffe0f903ce0b03cb3025ca
vishnu2848 <58807806+vishnu2848@users.noreply.github.com>
2020-11-07 22:34:02 +0530

    Update pipeline.py

--- pipesandfilters/pipeline.py
+++ pipesandfilters/pipeline.py
@@ -2,7 +2,7 @@ import abc
 import typing
 from multiprocessing import Process
 from .filter import Filter
-from .pipe import Pipes
+from .pipe import Pipe
 
 class Pipeline(object, metaclass=abc.ABCMeta):
 
@@ -10,38 +10,33 @@ class Pipeline(object, metaclass=abc.ABCMeta):
     Abstract base Pipeline class which receives a message and preforms required processing.
     '''
     def __init__(self, id: str):
-        self.id= ""
+        self.id= id
         self.source = None
-        self.sink = None
-        self.connections=dict()
-        self.filters=[]
-    
+        self.sink = []
+        
     def setSourceFilter(self, sourcefill:Filter):
         self.source = sourcefill
     def setSinkFilter(self, sinkfil:Filter):
         self.sink = sinkfil
     def getSourceFilter(self):
         return self.source 
-    def getsinkFilter(self):
+    def getSinkFilter(self):
         return self.sink
-    def addfilter(self,beforefilter:Filter, currentfilter:Filter):
-        if(beforefilter not in self.filters):
-            self.filters.append(beforefilter)
-        if(currentfilter not in self.filters):
-            self.filters.append(currentfilter)
-        if beforefilter not in self.connections:
-            self.connections[beforefilter.id] = [currentfilter.id]
-        else:
-            self.connections[beforefilter.id].append(currentfilter.id)
-    def connect(self):
-        for i in self.connections:
-            for k in self.filters:
-                if(i==k.id):
-                    outpipes = k.outgoingpipes
-            for j in self.connections[i]:
-                for k in self.filters:
-                    if(j==k.id):
-                        k.incomingpipes = outpipes 
-                        
-                
+    def validategraph(self):
+        visited = [] 
+        queue = [] 
+
+        queue.append(self.getSourceFilter())
+  
+        while queue: 
+            f = queue.pop(0) 
+            visited.append(f)
+            for pi in f.getOutgoingPipes(): 
+                if pi.getOutgoingFilter() not in visited and pi.getOutgoingFilter() not in self.getSinkFilter(): 
+                    queue.append(pi.getOutgoingFilter())
+                else:
+                    print("Not Feasible")
+                    return
+    def start(self):
+        print("Start")                
                 
