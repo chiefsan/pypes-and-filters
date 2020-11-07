63617df50435cfce6e4bead3873819fe957fa382
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-11-07 17:28:18 +0530

    create tests

---
+++ pipesandfilters/tests/simple_connection.py
@@ -0,0 +1,9 @@
+from pipesandfilters import Filter, SourceFilter, SinkFilter
+sink = SinkFilter(1,abs)
+source = SourceFilter(1, abs)
+from multiprocessing import Pipe
+reader,writer = Pipe()
+sink.setIncomingConnections([reader])
+source.setOutgoingConnections([writer])
+source.run(24)
+sink.run()
