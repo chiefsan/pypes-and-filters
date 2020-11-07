1b185dca7aa7ef7b3cbb23eec169a4a147da5f1f
Midhilesh29 <midhileshgithub@gmail.com>
2020-11-07 17:19:18 +0530

    Created base and derived class

--- pipesandfilters/pipe.py
+++ pipesandfilters/pipe.py
@@ -1,5 +1,23 @@
 import abc
-class Pipe(object, metaclass=abc.ABCMeta):
+from multiprocessing import Pipe
+class BasePipe(object, metaclass=abc.ABCMeta):
+    # Abstract class for pipes
+    def __init__(self,strategy):
+        self.strategy = strategy
     @abc.abstractmethod
-    def __str__(self):
-        raise NotImplementedError('users must define __str__ to use this base class')
+    def receive(self):
+        raise NotImplementedError('users must define receive to use this base class')
+    @abc.abstractclassmethod
+    def send(self,message):
+        raise NotImplementedError('users must define send to use this base class')
+
+
+class Pipes(BasePipe):
+	def __init__(self,strategy):
+		self.Inqueue,self.Outqueue = Pipe()
+		self.Strategy = strategy
+	def receive():
+        return self.Inqueue.recv()
+	def send(Message):
+		Message = self.Strategy.getMessage(Messages)
+		self.Outqueue.send(self.Messages)
