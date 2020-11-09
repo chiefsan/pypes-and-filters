39469d6d5aa22d9cf11fe92da013137d38cc24ad
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-11-10 00:47:47 +0530

    update docs

--- pipesandfilters/__init__.py
+++ pipesandfilters/__init__.py
@@ -1,3 +1,29 @@
+'''
+_Pypes and Filters_ is a package for implementing the pipes and filters architectural pattern.
+
+## Install
+
+It is recommended to run Pypes and Filters in a conda environment. For instructions on how to install conda go to this link: https://docs.anaconda.com/anaconda/install/
+
+With conda installed, run these commands to create a new conda environment and download and install Pypes and Filters:
+
+```shell
+git clone https://github.com/chiefsan/pypes-and-filters.git
+cd pypes-and-filters
+conda env create
+conda activate pypes-and-filters-env
+pip install .
+```
+
+**Use Pypes and Filters without conda**
+
+```shell
+git clone https://github.com/chiefsan/pypes-and-filters.git
+cd pypes-and-filters
+pip install .
+```
+'''
+
 import abc
 from .pipe import Pipe
 from .message import Message
