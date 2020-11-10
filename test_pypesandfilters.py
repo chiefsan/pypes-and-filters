4247412bec73f7c46113db918e229103545b2b3a
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-11-10 16:18:58 +0530

    update test structure

---
+++ test_pypesandfilters.py
@@ -0,0 +1,16 @@
+from pipesandfilters import Filter, SourceFilter, SinkFilter, Pipe, Pipeline
+
+def test1():
+    sink = SinkFilter("sink", abs)
+    filter = Filter(1, abs)
+    source = SourceFilter("source", abs)
+    p = Pipe("pipe", source, sink)
+    pipeline = Pipeline("lol")
+    pipeline.setSourceFilter(source)
+    pipeline.addSinkFilter(sink)
+    # pipeline.validate()
+    # pipeline.myGraphViz("/mnt/c/Users/sanja/Desktop/lol.PNG")
+    pipeline.run(2)
+
+def test2():
+    assert 2==2
