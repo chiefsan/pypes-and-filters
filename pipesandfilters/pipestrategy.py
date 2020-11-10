2399f4decd5880e3fb51b862fd4ea2463d53c0f6
Midhilesh29 <midhileshgithub@gmail.com>
2020-11-10 13:48:42 +0530

    updated the document

--- pipesandfilters/pipestrategy.py
+++ pipesandfilters/pipestrategy.py
@@ -1,47 +1,41 @@
 import abc
 
-
 class PipeStrategy(object, metaclass=abc.ABCMeta):
     """Abstract class for PipeStrategy
-    Pipestrategy class defines the blueprint for implementing the differnt pipeStrategy 
-    by which users can define how the message need to be transformed before sending to outgoingFilter.
-    """
 
+    Pipestrategy class defines the blueprint for implementing the differnt pipeStrategy 
+    by which user's can modify the order of the message before sending it to outgoinfFilter
+    """
     @abc.abstractclassmethod
-    def transformMessageQueue(self, messages):
+    def transformMessageQueue(self,messages):
         """
+        Defines the how messages need to be transformed before sending it to outgoingFilter
+
         Parameters
         ----------
-            messages : List
+            messages: List
                 Input queue got from incomingFilter
-
         Returns
-        -------
-            messages : List
-                Processed the incomingFilter message using user's logic
+        ------
+            messages: List
+                Processed message queue using user's logic
         """
-        raise NotImplementedError(
-            "children should implement it"
-        )
-
+        raise NotImplementedError("transformMessageQueue should be defined in base clase")
 
 class FIFO(PipeStrategy):
     """First In First Out strategy
 
-    It makes the message which is first received from the pipe through incomingFilter
-    should be sent first to the outgoingFilter.
+    It makes the first message from the incomingFilter to be processed first by the outgoingFilter 
+    and the last message from the incomingFilter to be processed last by the outgoinfFilter
     """
-
-    def transformMessageQueue(self, messages):
+    def transformMessageQueue(self,messages):
         return messages
-
 
 class LIFO(PipeStrategy):
     """Last in First Out strategy
 
-    It makes the message which is first received from the pipe through incomingFilter
-    should be sent last to the outgoingFilter    .
+    It makes the last message from the incomingFilter to be processed first by the outgoingFilter 
+    and the first message from the incomingFilter to be processed last by the outgoinfFilter
     """
-
-    def transformMessageQueue(self, messages):
+    def transformMessageQueue(self,messages):
         return messages[::-1]
